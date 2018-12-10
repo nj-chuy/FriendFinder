8b0b31fd5680fee3452178a43121abc5b389984d
jesus garcia <nj.chuy@gmail.com>
2018-12-09 21:57:15 -0500

    upload

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+
+	{
+	  "name": "Jared Leto",
+	  "photo": "https://akns-images.eonline.com/eol_images/Entire_Site/2018119/rs_634x1024-181209120714-634-jared-leto.cm.12918.jpg",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     2,
+	     1
+	   ]
+	},
+	{
+	  "name": "Elvis Presley",
+	  "photo": "https://www.rockhall.com/sites/default/files/styles/header_image_portrait/public/elvispresley-crop.jpg?itok=opERmP1o",
+	  "scores":[
+	     3,
+	     1,
+	     2,
+	     2,
+	     5,
+	     1,
+	     4,
+	     3,
+	     4,
+	     5
+	   ]
+	},
+	{
+	  "name": "BB King",
+	  "photo": "https://www.rockhall.com/sites/default/files/styles/header_image_portrait/public/bbking_001-3.jpg?itok=dxgZ4-nJ",
+	  "scores":[
+	     3,
+	     1,
+	     3,
+	     4,
+	     1,
+	     4,
+	     4,
+	     2,
+	     4,
+	     1
+	   ]
+	},
+]
+
+
+module.exports = friends;
